5fb4dca4de15dc90261a07e2a4c2b0bbef701cea
user <leesinyee1008@e.newera.edu.my>
2023-11-15 18:55:53 +0800

    add product 1,2,3,4,5 and product option 1,2,3,4,5 and reason return.

---
+++ app/src/main/java/com/example/computer_shop/product_option3.java
@@ -0,0 +1,14 @@
+package com.example.computer_shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class product_option3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_option3);
+    }
+}
