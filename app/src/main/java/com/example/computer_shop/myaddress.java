6195d4de5d851b8479d6962685c03aa1da6b4f8d
user <leesinyee1008@e.newera.edu.my>
2023-11-15 18:55:53 +0800

    update myadress,showdetailsmyaddress

--- app/src/main/java/com/example/computer_shop/myaddress.java
+++ app/src/main/java/com/example/computer_shop/myaddress.java
@@ -2,7 +2,12 @@ package com.example.computer_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class myaddress extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class myaddress extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myaddress);
+
+        Spinner s1;
+        s1=(Spinner)findViewById(R.id.spinner);
+        String[] address ={"Home","Office"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,address);
+        s1.setAdapter(adapter);
+    }
+    public void save(View view) {
+        Spinner s1;
+        s1= (Spinner) findViewById(R.id.spinner);
+        s1.getSelectedItem().toString();
+        EditText username = findViewById(R.id.username);
+        EditText contact = findViewById(R.id.contact);
+        EditText city = findViewById(R.id.city);
+        EditText street = findViewById(R.id.street);
+        EditText unit = findViewById(R.id.unit);
+        Intent intent = new Intent(this, showdetailsmyaddress.class);
+        intent.putExtra("username", username.getText().toString());
+        intent.putExtra("contact", contact.getText().toString());
+        intent.putExtra("city", city.getText().toString());
+        intent.putExtra("street", street.getText().toString());
+        intent.putExtra("unit", unit.getText().toString());
+        intent.putExtra("address",s1.getSelectedItem().toString());
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/computer_shop/showdetailsmyaddress.java
+++ app/src/main/java/com/example/computer_shop/showdetailsmyaddress.java
@@ -2,7 +2,11 @@ package com.example.computer_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class showdetailsmyaddress extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class showdetailsmyaddress extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showdetailsmyaddress);
+
+        Intent intent = getIntent();
+
+        TextView a =findViewById(R.id.username);
+        TextView b =findViewById(R.id.contact);
+        TextView c =findViewById(R.id.city);
+        TextView d =findViewById(R.id.street);
+        TextView e =findViewById(R.id.unit);
+        TextView f = findViewById(R.id.textView49);
+
+        String username =intent.getStringExtra("username");
+        String contact = intent.getStringExtra("contact");
+        String city =intent.getStringExtra("city");
+        String street = intent.getStringExtra("street");
+        String unit = intent.getStringExtra("unit");
+        String address = intent.getStringExtra("address");
+
+
+        a.setText(username);
+        b.setText(contact);
+        c.setText(city);
+        d.setText(street);
+        e.setText(unit);
+        f.setText(address);
+
+        Toast.makeText(this,username, Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this,contact, Toast.LENGTH_SHORT).show();
+
+
+    }
+    public void cancel(View view) {
+        Intent intent = new Intent(this,myaddress.class);
+        startActivity(intent);
     }
 }
