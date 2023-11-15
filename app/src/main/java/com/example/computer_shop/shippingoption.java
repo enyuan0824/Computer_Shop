b47658b191c17da385142d433e92682488dc250a
Shi Rong <shirongyew0329@gmail.com>
2023-11-15 16:54:31 +0800

    myaddress,myaddressdetails,accountinformation,accountinformationdetails,shippingoption,toreceive,profile

---
+++ app/src/main/java/com/example/computer_shop/shippingoption.java
@@ -0,0 +1,14 @@
+package com.example.computer_shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shippingoption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shippingoption);
+    }
+}
